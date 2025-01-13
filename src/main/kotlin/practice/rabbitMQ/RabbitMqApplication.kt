038886c547f5e105d8d99ffde93674b2a031f98b
tae.y <0211ilyoil@gmail.com>
2025-01-13 23:36:48 +0900

    setting :: project setting

---
+++ src/main/kotlin/practice/rabbitMQ/RabbitMqApplication.kt
@@ -0,0 +1,11 @@
+package practice.rabbitMQ
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RabbitMqApplication
+
+fun main(args: Array<String>) {
+	runApplication<RabbitMqApplication>(*args)
+}
