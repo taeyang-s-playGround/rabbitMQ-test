b452b85fb095ee0d8e509e8747f6b11f70332470
tae.y <0211ilyoil@gmail.com>
2025-01-17 00:55:23 +0900

    chore :: 쓸모없는 dependency 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,21 +22,15 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
+/*
 	implementation("io.jsonwebtoken:jjwt-api:0.10.7")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.10.7")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.10.7")
 
-	//coroutines
-	implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-	implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.0")
 
-	//open api 파싱
-	implementation ("com.google.code.gson:gson:2.8.6")
-	implementation ("org.json:json:20210307")
-
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+ */
+	//implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	//implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
@@ -47,10 +41,10 @@ dependencies {
 	implementation ("com.amazonaws:aws-java-sdk-s3:1.12.281")
 
 	//fcm
-	implementation("com.google.firebase:firebase-admin:8.1.0")
+	//implementation("com.google.firebase:firebase-admin:8.1.0")
 
 	//mysql
-	runtimeOnly("com.mysql:mysql-connector-j")
+	//runtimeOnly("com.mysql:mysql-connector-j")
 
 	//lombok
 	compileOnly("org.projectlombok:lombok")
