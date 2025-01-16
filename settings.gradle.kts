4af4c73de252a76b9a521662e6bceb75e7748acc
tae.y <0211ilyoil@gmail.com>
2025-01-17 00:55:56 +0900

    chore :: 파일명 수정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "rabbitmq"
+rootProject.name = "rabbitMQ"
--- src/test/kotlin/practice/rabbitMQ/RabbitMqApplicationTests.kt
+++ src/test/kotlin/practice/rabbitMQ/RabbitMqApplicationTests.kt
@@ -1,10 +1,10 @@
-package practice.rabbitmq
+package practice.rabbitMQ
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class RabbitmqApplicationTests {
+class RabbitMqApplicationTests {
 
 	@Test
 	fun contextLoads() {
